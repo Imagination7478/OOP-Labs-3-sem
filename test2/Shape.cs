9a1390f7f67ade49bef5c6ccc5e0be904825982c
Dmitry <43934773+Imagination7478@users.noreply.github.com>
2019-12-19 02:37:25 +0300

    Have done test

---
+++ test2/Shape.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Circle
+namespace OOP_test_2
+{
+    public struct Vector
+    {
+        public double x, y;
+
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x + v2.x, v1.y + v2.y);
+        }
+    }
+    public class Shape
+    {
+        //Center
+        private double x, y;
+        private double radius;
+
+        public Shape(double x, double y, double radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        void showCoords() {
+            Console.WriteLine($"Center: x = {x}, y = {y}");
+        }
+
+        void showRadius() {
+            Console.WriteLine($"Radius is x = {radius}");
+        }
+
+        public override string ToString()
+        {
+            return $"(x = {x}, y = {y})";
+        }   
+
+        public void move(Vector vector)
+        {
+            this.x += vector.x;
+            this.y += vector.y;
+        }
+
+        public double getPer()
+        {
+            return Math.PI * 2 * radius;
+        }
+
+        public double getArea()
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine(getInfo());
+        }
+
+        public string getInfo()
+        {
+            string centre = $"Centre: x = {x}, y = {y}";
+            string rad = $"Radius: {radius}";
+            return $"{centre}\n{rad}";
+            //return centre + "\n" + rad;
+        }
+    }
+}
---
+++ test2/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OOP_test_2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Shape circle = new Shape(3, 5, 7);
+        
+        [TestMethod]
+        public void OutputTest()    
+        {
+            string expected = "Centre: x = 3, y = 5\nRadius: 7";
+            string result = circle.getInfo();
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AreaTest()
+        {
+            double result = circle.getArea();
+            Assert.AreEqual(49 * Math.PI, result);
+        }
+
+        [TestMethod]
+        public void PerimetreTest()
+        {
+            double result = circle.getPer();
+            Assert.AreEqual(14 * Math.PI, result);
+        }
+
+        [TestMethod]
+        public void MoveTest()
+        {
+            Shape localCircle = new Shape(0, 0, 7);
+            localCircle.move(new Vector(12, 12));
+            string expected = "Centre: x = 12, y = 12\nRadius: 7";
+            string result = localCircle.getInfo();
+            Assert.AreEqual(expected, result);
+        }
+        
+    }
+}
